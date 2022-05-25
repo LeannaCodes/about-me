b4c887089abe45fe6f5b1cdca33f53fe5c143567
Leanna Lucas <leannalucas10@gmail.com>
2022-05-25 16:36:36 +0100

    add lab03 changes to script

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # class-02
+
+## Lab-03
+
+changes we have made: 
+
+- create a top 10 ordered list of your favourite movies. 
+- convert your work experience and education into unordered lists. 
+- add a 6th question to your guessing game that takes in numeric input by prompting user to guess a number. 
+- indicates through alert if the guess is too high/too low. 
+- should give the user 4 opportunities to get the correct answer 
+- after all attempts, tell user the correct answer. 
+- add 7th questions that has multiple possible correct answers that are stored in an array. 
+- give user 6 attempts to get it right.
+- display all possible correct answers to the user. 
+- consider using a loop for this. 
+- keep track of total number of correct answers out of the 7 questions asked. 
+- continue to use console.log at all times. 
+
+User story: 
+
+1. View more information about the site owner. 
+2. Be guided to an answer through a series of feedback responses to learn more about the site owner. 
+3. Guess the answer to a question that has multiple correct answers. 
+4. Know my final score so that I can know how well I did.
--- app.js
+++ app.js
@@ -1,55 +1,93 @@
-//This function is to get the users name
-function getName() {
+let myName;
 
-    let myName = prompt("What is your name?");
-    alert("Thank you, " + myName + " . Welcome to the Leannatrix");
+//This function is to get the users name.
+function getName() {
+  myName = prompt("What is your name?");
+  alert("Thank you, " + myName + " . Welcome to the Leannatrix");
+  console.log("myName");
 }
 
 //This function asks the user if they would like to play a game.
-function askAboutGame()
-
-let gameResult = confirm(myName + "would you like to play a game?");
-if (gameResult == true) {
+function askAboutGame() {
+  let gameResult = confirm(myName + "would you like to play a game?");
+  if (gameResult == true) {
     alert("wooo! Let's get started, " + myName + ". Make sure to answer the questions with a yes or a no.");
- 
+  }
 
-let year = prompt("Was Leanna born in 1995?");
-   
-    if (year == "yes" || "y") {
-    alert("No, she wasn't! Try again");
-
-    } else if (year == "no" || "n") {
-        alert("Well done! Leanna was born in 1997, not 1995.");
+    // This function lets you play a guessing game. 
+    function guessingGame() {
+        let year = prompt("Was Leanna born in 1995?");
+        while (year !== "yes" && year !== "y" && year !== "no" && year !== "n") {
+      alert("Please answer yes or no.");
+      year = prompt("Was Leanna born in 1995?");
+    }
+    if (year == "yes" || year == "y") {
+      alert("No you muppet!");
     } else {
-        alert("Please answer yes or no.");
+      alert("Well done! Leanna was born in 1997, not 1995.");
     }
 
-let placeOfBirth = prompt("Was Leanna born in Romford?");
-   
+    let placeOfBirth = prompt("Was Leanna born in Romford?");
+
+    while (
+      placeOfBirth !== "yes" &&
+      placeOfBirth !== "y" &&
+      placeOfBirth !== "no" &&
+      placeOfBirth !== "n"
+    ) {
+      alert("Please answer yes or no.");
+      placeOfBirth = prompt("Was Leanna born in Romford?");
+    }
+
     if (placeOfBirth == "yes" || "y") {
-    alert("Well done, Leanna was unfortunately born in Romford.");
-
-    } else if (placeOfBirth == "no" || "n") {
-        alert("Leanna wishes, please try again.");
+      alert("Well done, Leanna was unfortunately born in Romford.");
     } else {
-        alert("Please answer yes or no.");
+      alert("Leanna wishes, she was unfortunately born in Romford.");
     }
 
-let awesomeAnswer = prompt("Is Leanna the most awesome person in the world?");
-   
-    if ( awesomeAnswer == "yes" || "y") {
-    alert("Hell yeaaaaaah, that is correct.");
+    let awesomeAnswer = prompt(
+      "Is Leanna the most awesome person in the world?"
+    );
 
-    } else if (awesomeAnswer == "no" || "n") {
-        alert("Get the hell off of my page...NOW!");
+    while (
+      awesomeAnswer !== "yes" &&
+      awesomeAnswer !== "y" &&
+      awesomeAnswer !== "no" &&
+      awesomeAnswer !== "n"
+    ) {
+      alert("Please answer yes or no.");
+      awesomeAnswer = prompt("Is Leanna the most awesome person in the world?");
+    }
+
+    if (awesomeAnswer == "yes" || "y") {
+      alert("Hell yeaaaaaah, that is correct.");
     } else {
-        alert("Please answer yes or no.");
-    }    
+      alert("Get the hell off of my page...NOW!");
+    }
 
-} else {
+    let numberAnswer = 2
+    let numberGuess = prompt("How many years has Leanna had her cat Bear? Guess a number.");
+    while(numberAnswer != 2) {
+    alert("Go higher!");
+
+
+
+    }
+
+
+    //This is where they go if they don't want to play
+  } else {
     alert("Wow, ok then...Bye " + myName);
-} 
+  }
+}
+getName();
+askAboutGame();
 
-getName ();
-askAboutGame ();
+
+
+
+
+
+
+
 
